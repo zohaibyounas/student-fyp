6655b53dc2942988010fd4821e939b6cda843478
zohaibyounas <zohaiby737@gmail.com>
2026-05-12 01:43:01 +0500

    fix the gimini api key issue

--- run_with_db.bat
+++ run_with_db.bat
@@ -1,4 +1,4 @@
 @echo off
 set DATABASE_URL=postgresql://islamic_dream_db_user:cWM5ytIu8WIQiKDCk6jgHGI6CY5B6Yle@dpg-d7seo1t0lvsc73fnnlg0-a.singapore-postgres.render.com/islamic_dream_db
-set GEMINI_API_KEY=AIzaSyD-56uwDnGk3SsMTqWjkx9p8TDwkKUfD_A
+set GEMINI_API_KEY=AIzaSyCwaB7S_cRp3kOjgm5BEYJVa8rnvE-uZqc
 python app.py
