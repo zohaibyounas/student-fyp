2960fcd74f5402a22248e9952dabc34a8e30efb6
zohaibyounas <zohaiby737@gmail.com>
2026-05-15 15:18:25 +0500

    change the api key

--- run_with_db.bat
+++ run_with_db.bat
@@ -1,4 +1,5 @@
 @echo off
 set DATABASE_URL=postgresql://islamic_dream_db_user:cWM5ytIu8WIQiKDCk6jgHGI6CY5B6Yle@dpg-d7seo1t0lvsc73fnnlg0-a.singapore-postgres.render.com/islamic_dream_db
-set GEMINI_API_KEY=AIzaSyBTIIzADOvEbWtz_qgVQZVGwofCud_GIlQ
+set GEMINI_API_KEY=AIzaSyCEvuQXkyyUPy5Oj3BNeSlROhhbjs7iqCo
+
 python app.py
